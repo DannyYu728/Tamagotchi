ad613c6a2dd75e4a13ad95ee68ee14d2d4d04f11
Danny Yu <tiramisu@Dannys-MacBook-Pro.local>
2022-10-04 15:04:14 -0400

    tiume to commit

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const sheep = "https://64.media.tumblr.com/bc0b793c5949fbf57dc342422da1da28/tumb
 //     console.log("Apple Basket is full")
 //   }
 // }
-
+//----------------------Class----------------------------
 class template {
   constructor({ tag = 'div', className = '' } = {}) {
     this.element = document.createElement(tag);
@@ -75,7 +75,7 @@ class CatShip extends template {
     if (this.fireRate) {
       this.fireRate = false
       fireLaser({
-        x: kitty.x + 15,   
+        x: kitty.x + 15,
         y: kitty.y,
       })
       setTimeout(() => {
@@ -105,16 +105,29 @@ class Monster extends template {
     this.element.src = boo;
     this.setX(x);
     this.setY(y);
+    this.direction = 'left'
+  }
+  setDirectionLeft() {
+    this.direction = 'left'
+  }
+  setDirectionRight() {
+    this.direction = 'right'
+  }
+  moveDown() {
+    this.setY(this.y + 5)
+  }
+  update() {
+    if (this.direction === 'left') {
+      this.setX(this.x - 5)
+    } else {
+      this.setX(this.x + 5)
+    }
   }
 }
-
-
-
-
-
-
+//-----------------------Monsters--------------------------------//
+//Create Monster
 const spaceMons = []
-for (let r = 0; r < 3; r++){
+for (let r = 0; r < 3; r++) {
   for (let c = 0; c < 6; c++) {
     const spaceMon = new Monster({
       x: c * 100 + 350,
@@ -123,8 +136,20 @@ for (let r = 0; r < 3; r++){
     spaceMons.push(spaceMon);
   }
 }
-
-
+//-----------------Monster Movement--------------------
+const getLeftMon = () => {
+  return spaceMons.reduce((minSpaceMon, currentspaceMon) => {
+    return currentspaceMon.x < minSpaceMon.x
+      ? currentspaceMon : minSpaceMon;
+  })
+}
+const getRightMon = () => {
+  return spaceMons.reduce((maxSpaceMon, currentspaceMon) => {
+    return currentspaceMon.x > maxSpaceMon.x
+      ? currentspaceMon : maxSpaceMon;
+  })
+}
+//------------------Cat----------------------------
 const kitty = new CatShip()
 const lasers = []
 const fireLaser = ({ x, y }) => {
@@ -134,6 +159,7 @@ const fireLaser = ({ x, y }) => {
       y,
     }))
 }
+//-----------------Keys-------------------------
 const keys = {
   a: false,
   d: false,
@@ -145,6 +171,8 @@ document.addEventListener('keydown', e => {
 document.addEventListener('keyup', e => {
   keys[e.key] = false;
 })
+
+//-----------------------------Game Update Logic----------------
 const update = () => {
   if (keys[`a`] && kitty.x > 0) {
     kitty.moveLeft()
@@ -164,5 +192,22 @@ const update = () => {
       lasers.splice(lasers.indexOf(laser), 1);
     }
   });
+  spaceMons.forEach(spaceMon => {
+    spaceMon.update()
+  })
+  const leftMon = getLeftMon();
+  if (leftMon.x < 0) {
+    spaceMons.forEach(spaceMon => {
+      spaceMon.setDirectionRight();
+      spaceMon.moveDown();
+    })
+  }
+  const rightMon = getRightMon();
+  if (rightMon.x > window.innerWidth - 80) {
+    spaceMons.forEach(spaceMon => {
+      spaceMon.setDirectionLeft();
+      spaceMon.moveDown();
+    })
+  }
 };
 setInterval(update, 20)
