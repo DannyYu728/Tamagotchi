452c2c402c5989e2c4a0dbd63b6f4b66d2a1272a
Danny Yu <tiramisu@Dannys-MacBook-Pro.local>
2022-10-04 11:05:48 -0400

    change to use class cuz better

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const gameModal = document.querySelector(".gameModal")
-const cat = document.querySelector(".cat")
-const boss = document.querySelector(".boss")
+// const gameModal = document.querySelector(".gameModal")
+// const cat = document.querySelector(".cat")
+// const boss = document.querySelector(".boss")
 // let healthMeter = 100
 // let hungryMeter = 100
 // let happyMeter = 100
@@ -39,68 +39,123 @@ const boss = document.querySelector(".boss")
 //   }
 // }
 
-document.addEventListener('keydown', e => {
-  if (0 < cat.offsetLeft) {
-    if (e.code === 'KeyA') {
-      // cat.style.left = `${(cat.offsetLeft) - 50}px`;
-      cat.style.left = `${0}px`;
-    }
+class template  {
+  constructor({tag = 'div', className = ''} = {}) {
+    this.element = document.createElement(tag);
+    document.body.appendChild(this.element);
+    this.element.className = `template` + className;
   }
-})
-document.addEventListener('keydown', e => {
-  if (500 > cat.offsetLeft) {
-    if (e.code === 'KeyD') {
-      // cat.style.left = `${(cat.offsetLeft) + 50}px`;
-      cat.style.left = `${450}px`;
-    }
+  setX(x) {
+    this.x = x;
+    this.element.style.left = `${this.x}px`;
   }
-})
-
-
-let catLaser = document.createElement("div")
-let createCatLaser = () => {
-  catLaser.classList.add("catLaser")
-  cat.appendChild(catLaser)
-  catLaser.style.top = '0px'
-}
-
-document.addEventListener('keydown', e => {
-  if (e.code === 'Space') {
-    createCatLaser()
+  setY(y) {
+    this.y = y;
+    this.element.style.top = `${this.y}px`;
   }
-})
-let moveCatLaser = () => {
-  catLaser.style.top = `-${gameModal.offsetHeight}px`;
-}
-setInterval(moveCatLaser, 1000)
 
-let bossLaser = document.createElement("div")
-let createBossLaser = () => {
-  bossLaser.classList.add("bossLaser")
-  boss.appendChild(bossLaser)
-  bossLaser.style.top = '0px'
 }
-setInterval(createBossLaser, 1000)
 
-let moveBossLaser = () => {
-  bossLaser.style.top = `${gameModal.offsetHeight}px`;
-}
-setInterval(moveBossLaser, 1000)
-
-let moveBoss = () => {
-  boss.style.left = `${Math.floor(Math.random() * 500)}px`;
-}
-setInterval(moveBoss, 2000)
-
-class catShip {
+class catShip extends template {
   constructor() {
-    this.cat = document.createElement('img')
-    this.cat.src = "https://64.media.tumblr.com/692589ab5105831fd3e3897553979fc4/tumblr_npyi0xoh601tl2vzco1_500.gifv"
-    document.body.appendChild(this.cat)
-    this.cat.className = 'cat'
+    this.element = document.createElement('img');
+    this.element.src = "https://64.media.tumblr.com/692589ab5105831fd3e3897553979fc4/tumblr_npyi0xoh601tl2vzco1_500.gifv";
+    document.body.appendChild(this.element);
+    this.element.className = 'cat';
+    this.setX(window.innerWidth / 2);
+    this.setY(window.innerHeight - 100);
+    this.fireRate = true
+  }
+  setX(x) {
+    this.x = x;
+    this.element.style.left = `${this.x}px`;
+  }
+  setY(y) {
+    this.y = y;
+    this.element.style.top = `${this.y}px`;
+  }
+  moveLeft() {
+    this.setX(this.x - 5)
+  }
+  moveRight() {
+    this.setX(this.x + 5)
+  }
+  shoot({ fireLaser }) {
+    if (this.fireRate) {
+      this.fireRate = false
+      fireLaser({
+        x: kitty.x + 25,
+        y: kitty.y,
+      })
+      setTimeout(() => {
+        this.fireRate = true
+      }, 500)
+    }
   }
 }
 
-let kitty = new catShip()
-
-
+class catLaser {
+  constructor({ x, y }) {
+    this.element = document.createElement('div');
+    document.body.appendChild(this.element);
+    this.element.className = 'catLaser';
+    this.setX(x);
+    this.setY(y);
+  }
+  setX(x) {
+    this.x = x;
+    this.element.style.left = `${this.x}px`;
+  }
+  setY(y) {
+    this.y = y;
+    this.element.style.top = `${this.y}px`;
+  }
+  updateUp() {
+    this.setY(this.y - 5);
+  }
+  remove() {
+    this.element.remove();
+    this.element = null;
+  }
+}
+const kitty = new catShip()
+const lasers = []
+const fireLaser = ({ x, y }) => {
+  lasers.push(
+    new catLaser({
+      x,
+      y,
+    }))
+}
+const keys = {
+  a: false,
+  d: false,
+  [' ']: false,
+}
+document.addEventListener('keydown', e => {
+  keys[e.key] = true;
+})
+document.addEventListener('keyup', e => {
+  keys[e.key] = false;
+})
+const update = () => {
+  if (keys[`a`] && kitty.x > 0) {
+    kitty.moveLeft()
+  }
+  if (keys[`d`] && kitty.x < window.innerWidth - 50) {
+    kitty.moveRight()
+  }
+  if (keys[` `]) {
+    kitty.shoot({
+      fireLaser
+    })
+  }
+  lasers.forEach(laser => {
+    laser.updateUp();
+    if (laser.y < 0) {
+      laser.remove()
+      lasers.splice(lasers.indexOf(laser), 1);
+    }
+  });
+};
+setInterval(update, 20)
